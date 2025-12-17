40cce1d47c0515d5a3383c7d530a8adf66755484
shanikumar91 <krishna8791568821@gmail.com>
2025-12-17 21:38:00 +0530

    Initial commit

---
+++ UserService/src/main/java/com/lcwd/user/service/external/services/HotelService.java
@@ -0,0 +1,13 @@
+package com.lcwd.user.service.external.services;
+
+import com.lcwd.user.service.entities.Hotel;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "HOTEL-SERVICE")
+public interface HotelService {
+      // this method() implementation  automatic at the Runtime  and itself call
+    @GetMapping("/hotels/{hotelId}")
+     public Hotel getHotel(@PathVariable("hotelId") String hotelId);
+}
